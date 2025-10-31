c6b090277078ede00885e72aff40be46c54d7f07
Alex <blazezombie27@gmail.com>
2025-10-31 00:52:24 +0000

    uhhhhhhh i added some stuff? didnt test tho, i suck at ts

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
 extern crate eframe;
 use eframe::egui;
 struct App {
-
+    output: i32,
 }
 
 impl Default for App {
     fn default() -> Self {
         Self{
-
+            output: 0,
         }
     }
 }
 
 impl eframe::App for App{
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame){
-
+        let Self { output } = self;
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label(self.output.to_string());
+        });
+        
     }
 }
 
